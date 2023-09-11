4b30033a3ec530ee01ab87afe5a450c1b151d9cf
John Alarcon <j.alarcon@outlook.com>
2023-09-11 19:02:27 -0400

    added function that renders components and listed imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
+import Header from '';
+import PortfolioContainer from '';
+import Footer from '';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('About');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+      <PortfolioContainer
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <Footer/>
     </div>
   );
-}
+};
 
 export default App;
