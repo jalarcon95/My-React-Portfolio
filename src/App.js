a145fb493c66303f5695abf6f1fc230776e65c79
John Alarcon <j.alarcon@outlook.com>
2023-09-13 19:36:39 -0400

    add files for pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useState} from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
-import Header from '';
-import Footer from '';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 const App = () => {
