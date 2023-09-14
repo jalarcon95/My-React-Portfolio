926c0c27555a0b8e6e45b7f8fd70949f09aad286
John Alarcon <j.alarcon@outlook.com>
2023-09-14 10:41:33 -0400

    added styling and function that renders about page and exports

---
+++ src/components/pages/About/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './About.css';
+import profilepic from '../../assets/profilepic.jpg'
+import CardMedia from '@mui/material/CardMedia';
+
+export default function About() {
+    return (
+        <>
+            <div className='image-container'>
+                <CardMedia
+                    component='img'
+                    className='color-img'
+                    style={{
+                        height:'100px',
+                        width:'100px',
+                        margin: '0 auto',
+                        paddingTop: '20px',
+                    }}
+                    image={profilepic}
+                    alt='About Me'
+                /> 
+            </div>
+
+            <div style={{width: '80%', margin: '0 auto'}}>
+                <h1 id='about-me-title'>About Me</h1>
+                <p id='p-about'>
+
+                </p>
+                <p id='p-about'>
+                    
+                </p>
+                <p id='p-about'>
+                    
+                </p>
+            </div>
+        </>
+    );
+}
