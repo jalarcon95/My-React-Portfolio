a8e41155c381facee8243dfbfe50986c524de3b6
John Alarcon <j.alarcon@outlook.com>
2023-09-15 15:20:06 -0400

    added image links to projects array

--- src/projects.js
+++ src/projects.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         projectTitle: '5-Day Weather Forecast',
         description: '',
-        imageLink: '',
+        imageLink: 'https://raw.githubusercontent.com/jalarcon95/My-React-Portfolio/main/src/components/assets/5-Day-Weather-Forecast.png',
         liveLink: 'https://jalarcon95.github.io/5-Day-Weather-Forecast/',
         gitHub: 'https://github.com/jalarcon95/5-Day-Weather-Forecast',
     },
@@ -10,7 +10,7 @@ export const projects = [
     {
         projectTitle: 'My Workday Scheduler',
         description: '',
-        imageLink: '',
+        imageLink: 'https://raw.githubusercontent.com/jalarcon95/My-React-Portfolio/main/src/components/assets/My-Workday-Scheduler.png',
         liveLink: 'https://jalarcon95.github.io/My-Workday-Scheduler/',
         gitHub: 'https://github.com/jalarcon95/My-Workday-Scheduler',
     },
@@ -18,7 +18,7 @@ export const projects = [
     {
         projectTitle: 'Timed Coding Assessment',
         description: '',
-        imageLink: '',
+        imageLink: 'https://raw.githubusercontent.com/jalarcon95/My-React-Portfolio/main/src/components/assets/coding-quiz-start-page.png',
         liveLink: 'https://jalarcon95.github.io/Timed-Coding-Assessment/',
         gitHub: 'https://github.com/jalarcon95/Timed-Coding-Assessment',
     },
