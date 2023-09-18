75711e67cecc6ee530c91d5188d107cbd81b68f7
John Alarcon <j.alarcon@outlook.com>
2023-09-18 18:47:07 -0400

    added imports and function for resume component

---
+++ src/components/pages/Resume/Resume.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Resume.css';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import PdfFile from '';
+
+export default function Resume() {
+    return (
+        <Box>
+            <div>
+                <a 
+                    href={PdfFile}
+                    download='John D. Alarcon Resume'
+                    target='_blank'
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        paddingTop: '25px',
+                    }}
+                    rel='noreferrer'
+                >
+                    <Button id='resume-button' className='pop-on-hover'>
+                        Click Here to Download Resume
+                    </Button>
+                </a>
+            </div>
+            <h2
+                style={{
+                    display:'flex',
+                    justifyContent: 'center',
+                    textDecoration: 'underline',
+                }}
+            >
+                
+            </h2>
+        </Box>
+    )
+}
