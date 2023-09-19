b41bb27a25856ac11573f9b7640e71e517ec549c
John Alarcon <j.alarcon@outlook.com>
2023-09-18 20:14:42 -0400

    added details for About component

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -24,12 +24,24 @@ export default function About() {
             <div style={{width: '100%', margin: '0 auto'}}>
                 <h1 id='about-me-title'>About Me</h1>
                 <p id='p-about'>
-
+                    Hello! My name is John Alarcon and I am an aspiring web developer that is currently enrolled in the 
+                    UCF Full Stack Developer Bootcamp. I am excited to complete my career transition into the field of web development!
+                 
                 </p>
                 <p id='p-about'>
                     
+                    My background is mainly focused in sales, finance, client relationships, and customer service. I've worked in banking, electronics retail, and residential real estate.  
+                    I have a Bachelor's degree in Business Administration and a Florida Real Estate License. I've built a strong foundation in 
+                    problem-solving, working in teams, data analysis, relationship building, and time management. Throughout my work experience, I have always leveraged
+                    technology and have been comfortable working with various types of softwares like CRM, banking systems, email, MS Office, databases, mobile apps, etc.
+                    Thus is my desire to expand my technical skillet and pursue a career as a web developer. I have always considered myself as a fast learner and am excited with
+                    the prospect of learning and growing in the ever evolving world of tech.
                 </p>
                 <p id='p-about'>
+                    
+                    Outside of web development, I love to watch and play soccer. I enjoy playing weekly in a compeititve league with friends and I also enjoy 
+                    supporting Orlando's local MLS club OCSC! I enjoy traveling, playing videogames, and cooking for my family & friends. I am also involved in my local community
+                    by officiating in several different levels of youth soccer as a Certified Referee with USSF.
                     
                 </p>
             </div>
