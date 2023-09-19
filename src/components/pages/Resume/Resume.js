da8af35fa4148804b13489e5265005e0e252f583
John Alarcon <j.alarcon@outlook.com>
2023-09-18 20:37:39 -0400

    fix' git commit -m fix

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -28,7 +28,6 @@ export default function Resume() {
                 style={{
                     display:'flex',
                     justifyContent: 'center',
-                    textDecoration: 'underline',
                 }}
             >
                 
